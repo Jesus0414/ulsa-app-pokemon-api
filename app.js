2dee2a7fb936c3e637acc0698b9914c907da19cf
Jesus_0414 <jesus140400@gmail.com>
2019-09-18 09:56:23 -0700

    peso, altura, numero, y casi habilidades

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-const pokemonName= document.getElementById('pokemon-name');
+const pokemonName = document.getElementById('pokemon-name');
 const pokemonHeight = document.getElementById('pokemon-height');
+const pokemonFrontImg = document.getElementById('pokemon-img-front');
+const pokemonNumber = document.getElementById('pokemon-number');
+const pokemonWeight = document.getElementById('pokemon-weight');
+const pokemonAbilities = document.getElementById('pokemon-abilities'); 
+
 pokemonName.innerText = 'El nombre de mi pokémon es: ';
 
 const pokemonApiUrl = 'https://pokeapi.co/api/v2/';
 
 let getPokemonData = async ()=>{
-    const response = await fetch(`${pokemonApiUrl}pokemon/25/`);
-    const pokemondata = await response.json();
-    console.log(pokemondata);
-    const {name , height}= pokemondata;
+    const response = await fetch(`${pokemonApiUrl}pokemon/393/`);
+    const pokemon = await response.json();
+    console.log(pokemon);
+    const {name , height, abilities}= pokemon;
+    const frontImg = pokemon.sprites.front_default;
     pokemonName.innerText = name;
-    pokemonHeight.innerText = `la altura es: ${height} pulgadas`;
+    pokemonNumber.innerText = `El número de la pokédex es: ${pokemon.id}`;
+    pokemonHeight.innerText = `La altura es: ${height} pulgadas`;
+    pokemonWeight.innerText = `Su peso es de: ${pokemon.weight}kg`
+    pokemonFrontImg.src = frontImg;
+    abilities.forEach(ability => {
+        pokemonAbilities.innerHTML = `<li>${ability.name}</li>`;
+    });
+    
 };
 
 getPokemonData();
